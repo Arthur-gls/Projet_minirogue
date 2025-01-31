7e2106db7688cb5d4a7e950efe38d5105e492969
Basile <basile.dumas@etu.minesparis.psl.eu>
2025-01-31 11:24:53 +0100

    réorganisation du main

--- main.py
+++ main.py
@@ -31,9 +31,6 @@ class Sac(Objets):
         self.potions = []
 
 
-sac = Sac()
-print(sac.armes)
-
 # Données de l'arène
 
 LENGHT = 30
@@ -144,21 +141,25 @@ def move (key, position):
         next_move = x, y - 1
     return next_move
 
-WAIT = True
 
-while WAIT :
-    # Wait for the next event.
-    event = keyboard.read_event()
-    if event.event_type == keyboard.KEY_DOWN :
-        key = event.name
-        
-    next_move = move(key, position)
-    next_type = TYPES(map[next_move])
+def main():
+    sac = Sac()
+    sac_ouvert = False
+    WAIT = True
+    while WAIT :
+        # Wait for the next event.
+        event = keyboard.read_event()
+        if event.event_type == keyboard.KEY_DOWN :
+            key = event.name
+            
+        next_move = move(key, position)
+        next_type = TYPES(map[next_move])
 
-    if next_type in ('room', 'door', 'corridor', 'staircase'):
-        position = next_move
+        if next_type in ('room', 'door', 'corridor', 'staircase'):
+            position = next_move
 
-    else :
-        position = next_move
+        else :
+            position = next_move
    
 
+main()
