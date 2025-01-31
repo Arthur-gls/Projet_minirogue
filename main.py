c7ef5e3fe61b025560d5ad9a122651671185fbfe
Basile <basile.dumas@etu.minesparis.psl.eu>
2025-01-31 12:00:02 +0100

    réorganisation du code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import keyboard
+
 class Objets:
     arme_de_base = ["contact",1]
     épée = ["contact",2]
@@ -29,6 +30,7 @@ class Sac(Objets):
         self.armes = [Objets.arme_de_base]
         self.armures = []
         self.potions = []
+        self.sac_ouvert = False
 
 
 # Données de l'arène
@@ -129,37 +131,39 @@ TYPES = {'-' : 'wall', ' ': 'wall', '|' : 'wall', '.' : 'room', '#' : 'corridor'
 def get_position(arene):
     return position
 
-def move (key, position):
+def next_position(key, position):
     x, y = position
     if key == 'left' :
-        next_move = x - 1, y
+        next_position = x - 1, y
     if key == 'right' :
-        next_move = x + 1, y
+        next_position = x + 1, y
     if key == 'up' :
-        next_move = x, y - 1
+        next_position = x, y - 1
     if key == 'down' :
-        next_move = x, y - 1
-    return next_move
+        next_position = x, y + 1
+    return next_position
 
+def valid_move(key, position):
+    next_position = next_position(key, position)
+    next_type = TYPES(map[next_position])
+    if next_type not in ("wall"):
+        return True
+
+def event(key, position):
+    print("faire la suite")
 
 def main():
     sac = Sac()
-    sac_ouvert = False
+    joueur = Joueur(5,5)
     WAIT = True
     while WAIT :
         # Wait for the next event.
-        event = keyboard.read_event()
-        if event.event_type == keyboard.KEY_DOWN :
-            key = event.name
-            
-        next_move = move(key, position)
-        next_type = TYPES(map[next_move])
-
-        if next_type in ('room', 'door', 'corridor', 'staircase'):
-            position = next_move
-
-        else :
-            position = next_move
+        key_pressed = keyboard.read_event()
+        if key_pressed.event_type == keyboard.KEY_DOWN :
+            key = key_pressed.name
+        
+        if valid_move(key, position):
+            event(key, position)
    
 
 main()
